e3f86ac8ef61eff2a65c12ea9dbdc5fba659e412
Rashi Mathur <rashi@uoguelph.ca>
2021-11-03 21:10:44 -0400

    DONE, just need to make it more efficient

--- hash.c
+++ hash.c
@@ -29,7 +29,7 @@ struct HashTable *createTable( struct memsys *memsys,
 
 	//initializing all the values to memnull
 	for (int i=0; i<capacity; i++){
-		setval(memsys,&set, sizeof(int), data+(sizeof(int))*i );
+		setval(memsys,&set, sizeof(int), data+ sizeof(int)*i );
 	}
 
 	new->data = data;
@@ -38,84 +38,85 @@ struct HashTable *createTable( struct memsys *memsys,
 
 void addElement( struct memsys *memsys, struct HashTable *table, int addr )
 {
-	void* value1 = malloc(table->width);
-	void* value2 = malloc(sizeof(int));
-
+	void* value = malloc(table->width);
 	int index,i ,address;
+
 	if(table->nel==table->capacity){
 		fprintf(stderr, "ERROR: The table is completely filled\n");
 		exit(0);
 	}
-	getval(memsys,value1, table->width, addr );
-	index = table->hash(value1, (table->capacity)+1);
-	address = (table->data)+((sizeof(int))*index);
-	//getval(memsys,value2, sizeof(int), address);
+
+	getval(memsys,value, table->width, addr );
+	index = table->hash(value, (table->capacity));
+	getval(memsys, &address, sizeof(int),(table->data)+sizeof(int)*index );
+	
 	i=index;
+
 	while(address!=MEMNULL){
 		i++;
 		if(i==table->capacity){
 			i=0;
 		}
-		address = table->data+((sizeof(int))*i);
-		//getval(memsys,value2, table->width, address);
+	    getval(memsys, &address, sizeof(int),(table->data)+sizeof(int)*i );
 		if(i==index){
 			break;
-		}	
+		}
 	}
 	setval(memsys, &addr,sizeof(int), (table->data)+i*sizeof(int) );
 	table->nel = table->nel +1;
-	free(value1);
-	free(value2);
+	free(value);
 }
-//giving the wrong output
-int getElement( struct memsys *memsys, struct HashTable *table, void*key )
-{
-	int index, address, comparision, i;
+
+int getElement( struct memsys *memsys, struct HashTable *table, void*key ){
+	int index, address, i;
 	void* value = malloc(table->width);
-	//void* value2 = malloc(table->width);
-	//value2=NULL;
-	index = table->hash(key, (table->capacity)+1);
-	getval(memsys, &address, sizeof(int), (table->data)+index*sizeof(int));
-	if(address==MEMNULL){
+
+	index = table->hash(key, table->capacity);
+	getval(memsys, &address, sizeof(int), table->data+ index*sizeof(int));
+
+	/*if(address==MEMNULL){
 		free(value);
-		
-		return address;
-	}
+		return MEMNULL;
+	}*/
 
 	getval(memsys, value, table->width, address);
-	comparision = table->compar(value, key);
-
 	
-	i=index;
-	while((comparision!=0)&&(i<table->capacity)){
-		i++;
-
-		getval(memsys, &address, sizeof(int), (table->data)+i*sizeof(int));
-		if(address!=MEMNULL){
-			getval(memsys,value, table->width, address);
-
-		}
-
-		comparision = table->compar(value, key);
-
-	}
-
-	if(comparision==0){
+	if(table->compar(value, key)==0){
 		free(value);
-		//free(value2);
 		return address;
 	}
-	if(i==table->capacity){
-		i=0;
-		while((address!=MEMNULL)&&(i<=index)){
-			getval(memsys, &address, sizeof(int), (table->data)+i*sizeof(int));
+	else{
+		i=index;
+		while(i<table->capacity){
 			i++;
+			getval(memsys, &address, sizeof(int), (table->data)+i*sizeof(int));
+			if(address!=MEMNULL){
+				getval(memsys, value, table->width, address);
+				if(table->compar(value, key)==0){
+					free(value);
+					return address;
+				}
+			}
+			
 		}
+			i=0;
+			while(i<index){
+				getval(memsys, &address, sizeof(int), (table->data)+i*sizeof(int));
+				i++;
+				if(address!=MEMNULL){
+					getval(memsys, value, table->width, address);
+					if(table->compar(value, key)==0){
+						free(value);
+						return address;
+					}
+
+				}
+			}
 	}
 	free(value);
-	return address;
-	
+	return MEMNULL;
 }
+
 
 void freeTable( struct memsys *memsys, struct HashTable *table )
 {
@@ -127,27 +128,24 @@ int hashAccuracy( struct memsys *memsys, struct HashTable *table )
 {
 	int calculatedIndex, index, difference ,total=0, address =0;
 	void*value = malloc(table->width);
+
 	for(index = 0; index<table->capacity; index++){
 
 		getval(memsys, &address, sizeof(int), (table->data)+index*sizeof(int));
-//seg fault here
-		getval(memsys, value, table->width, address);
-	
+		if(address!=MEMNULL){
+			getval(memsys, value, table->width, address);
+			calculatedIndex = table->hash(value, (table->capacity));
+			difference = index - calculatedIndex;
+			if(difference>=0){
+				total += difference;
+			}
+			else{
+				total+=index -(table->capacity)+calculatedIndex;
+			}
 
-		calculatedIndex = table->hash(value, (table->capacity)+1);
-		difference = index - calculatedIndex;
-		if(difference>=0){
-			total += difference;
 		}
-		else{
-			total+=index +(table->capacity)-calculatedIndex;
-		}
+		
 	}
 	free(value);
 	return total;
 }
-
-
-
-
-
